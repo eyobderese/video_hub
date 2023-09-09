94f8c9c314372c9d238f883467fd3257cd7adc8c
Eyob Derese <eyobderese123@gmail.com>
2023-09-09 21:46:43 +0300

    build a responsive lay out

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, GridItem, Show } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav "  "aside  main"`,
+      }}
+    >
+      <GridItem area="nav" bg={"red"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg={"blue"}>
+          aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg={"yellow"}>
+        main
+      </GridItem>
+    </Grid>
   );
 }
 
